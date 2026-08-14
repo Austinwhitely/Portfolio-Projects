25799b2263e95eab8488a5e495e6fc70817b17f3
Austinwhitely <Austinwhitely1@gmail.com>
2026-08-14 18:02:57 -0400

    Command line parser (WIP) as of now it only takes one command at a time instead of multiple

--- 01-Expense Tracker/main.py
+++ 01-Expense Tracker/main.py
@@ -1,13 +1,36 @@
-import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument("echo", help="echo the string you use here")
-args = parser.parse_args()
-print(args.echo)
+from argparse import ArgumentParser, Namespace
+ID = 1
+expense_list = [["ID", "Date", "Description", "Amount"]]
 
-class expense_tracker:
+def main():
 
-    def init__(self):
-        self.expenses = []
+    parser = ArgumentParser(description= "Expense Tracker")
+    subparser = parser.add_subparsers(dest="command", help=" expense tracker")
 
-    #def add_expense():
+    addition_parser = subparser.add_parser("add", help=' adds expense to list')
+    addition_parser.add_argument("--description", type= str, required=True,help="Description of expense in a string")
+    addition_parser.add_argument("--amount", type= float, required=True, help= "cost of expense in dollars")
+    addition_parser.set_defaults(func=addition_func)
+
+    list_parser = subparser.add_parser("list", help="lists out all expenses")
+    list_parser.set_defaults(func=list_func)
+                                
+    # a namespace to call all of my arguments
+    args: Namespace = parser.parse_args()
+    args.func(args)
+
+def addition_func(args):
+ expense_list.append([ID, "01/20/10", "games", 10.0])
+
+def list_func(args):
+   for row in expense_list:
+      print("\t".join(row)) 
+
+ 
+
+
+if __name__ == "__main__":
+    main()
+    
+
