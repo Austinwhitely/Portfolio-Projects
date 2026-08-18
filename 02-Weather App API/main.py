60bf9aa8021d9096c3d021b88cc9226ae90863ae
Austinwhitely <Austinwhitely1@gmail.com>
2026-08-18 14:59:52 -0400

    api data retrieval

--- 02-Weather App API/main.py
+++ 02-Weather App API/main.py
@@ -1,21 +1,27 @@
-from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
-
+from fastapi import FastAPI, Query, HTTPException
+import requests
+#from app.config import WEATHER_API_KEY
+from urllib.parse import quote
 app = FastAPI()
 
-weather_data = []
+API_KEY = "apikey"
 
-class Weather(BaseModel):
-    zip: int 
-    high: int = None
-    low: int = None
-
-@app.get('/')
-def root():
-    return{"hello world"}
-
-@app.post('/zip')
-def create_item(item: Weather):
-    weather_data.append(item)
-    return weather_data
+@app.get('/weather')
+def get_weather(zip_code = Query("23607", examples=["23607"])):
+    
+    #encoded_zip = quote(zip_code)
+    
+    url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/"f"{zip_code}?unitGroup=us&key={API_KEY}&contentType=json"
+    
+    response = requests.get(url)
+    data = response.json()
+    
+    
+    
+    return{
+        "city": str(data["resolvedAddress"]),
+        "temperature": float(data["currentConditions"]["temp"]),
+        "humidity": float(data["currentConditions"]["humidity"]),
+        "description": str(data["currentConditions"]["conditions"])     
+    }
 
