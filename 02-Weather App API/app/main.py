ef7893a114884e7cab200042984692ec0cc2ee82
Austinwhitely <Austinwhitely1@gmail.com>
2026-08-21 22:31:57 -0400

    Added error handling

--- 02-Weather App API/app/main.py
+++ 02-Weather App API/app/main.py
@@ -12,14 +12,18 @@ API_KEY = WEATHER_API_KEY
 @app.get('/weather')
 def get_weather(zip_code = Query("23607", examples=["23607"])):
     
-    #encoded_zip = quote(zip_code)
+    if not WEATHER_API_KEY:
+        raise HTTPException(status_code=500, detail="API key not configured")
+    
     
     url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/"f"{zip_code}?unitGroup=us&key={API_KEY}&contentType=json"
     
     response = requests.get(url)
+    
+    if response.status_code != 200:
+        raise HTTPException(status_code=404, detail="Zip not found or weather service unavailable. Please confirm the Zip Code and try again.")
+    
     data = response.json()
-    
-    
     
     return{
         "city": str(data["resolvedAddress"]),
